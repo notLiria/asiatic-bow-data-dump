84849e41dbcfb15c61d5c77a1519dcd119d62ace
Liria <fdshu@uwaterloo.ca>
2023-05-29 21:51:25 -0400

    let write be false

--- utils/addBowCsv.js
+++ utils/addBowCsv.js
@@ -6,38 +6,40 @@ const { spawn } = require('child_process');
 async function addBowData(bowType, csvFile) {
   const jsonData = processBowCsv(csvFile);
 
-  // Check if the bow type directory exists, if not create it
-  const bowDir = path.join(__dirname, 'data', 'bows', bowType);
-  if (!fs.existsSync(bowDir)) {
-    fs.mkdirSync(bowDir, { recursive: true });
+  if (write === undefined) {
+    // Check if the bow type directory exists, if not create it
+    const bowDir = path.join(__dirname, 'data', 'bows', bowType);
+    if (!fs.existsSync(bowDir)) {
+      fs.mkdirSync(bowDir, { recursive: true });
+    }
+
+    const dataFile = path.join(bowDir, 'data.json');
+
+    let data = { samples: [] };
+    if (fs.existsSync(dataFile)) {
+      data = JSON.parse(fs.readFileSync(dataFile));
+    }
+
+    data.samples.push(jsonData);
+
+    fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
+
+    // Run the add-calcs script
+    const python = spawn('python', [
+      'path/to/your/python/calculations/updateData.py',
+    ]);
+    python.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+    python.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+    python.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
   }
-
-  const dataFile = path.join(bowDir, 'data.json');
-
-  let data = { samples: [] };
-  if (fs.existsSync(dataFile)) {
-    data = JSON.parse(fs.readFileSync(dataFile));
-  }
-
-  data.samples.push(jsonData);
-
-  fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
-
-  // Run the add-calcs script
-  const python = spawn('python', [
-    'path/to/your/python/calculations/updateData.py',
-  ]);
-  python.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-  python.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-  python.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
 }
 
 // Usage: node addBowData.js "Alibow New" "someRandomBow.csv"
-const [bowType, csvFile] = process.argv.slice(2);
-addBowData(bowType, csvFile);
+const [bowType, csvFile, write] = process.argv.slice(2);
+addBowData(bowType, csvFile, write);

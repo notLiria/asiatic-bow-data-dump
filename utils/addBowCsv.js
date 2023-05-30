45dcc50828c14c236b7805b634f5a88ce0c526e4
Liria <fdshu@uwaterloo.ca>
2023-05-29 21:43:59 -0400

    Refactor dfcurve

--- utils/addBowCsv.js
+++ utils/addBowCsv.js
@@ -4,7 +4,6 @@ const processBowCsv = require('./readBowCsv');
 const { spawn } = require('child_process');
 
 async function addBowData(bowType, csvFile) {
-  // Process the CSV file
   const jsonData = processBowCsv(csvFile);
 
   // Check if the bow type directory exists, if not create it
@@ -13,19 +12,15 @@ async function addBowData(bowType, csvFile) {
     fs.mkdirSync(bowDir, { recursive: true });
   }
 
-  // Path to the data.json file
   const dataFile = path.join(bowDir, 'data.json');
 
-  // Check if the data.json file exists, if not create it with an empty samples array
   let data = { samples: [] };
   if (fs.existsSync(dataFile)) {
     data = JSON.parse(fs.readFileSync(dataFile));
   }
 
-  // Add the new data to the samples array
   data.samples.push(jsonData);
 
-  // Write the updated data back to the data.json file
   fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
 
   // Run the add-calcs script

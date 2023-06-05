ffdc9d8b5ad0784ad763d2f1a6e8bd7562ef0db8
Liria <hudashing+github@gmail.com>
2023-06-04 23:35:34 -0400

    Update psql script

--- pgsql/initialization.sql
+++ pgsql/initialization.sql
@@ -1,50 +1,83 @@
--- BowTypes table
-CREATE TABLE BowTypes (
-   BowTypeID SERIAL PRIMARY KEY,
-   BowTitle VARCHAR(255) NOT NULL,
-   BowLink VARCHAR(255),
-   CONSTRAINT uk_bowtitle UNIQUE (BowTitle)
+-- bow_types table
+CREATE TABLE bow_types (
+   bow_type_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   model_name TEXT NOT NULL,
+   manufacturer TEXT, 
+   bow_link TEXT,
+   CONSTRAINT uk_model_name UNIQUE (model_name, manufacturer)
 );
 
--- Samples table
-CREATE TABLE Samples (
-   SampleID SERIAL PRIMARY KEY,\
-   BowTypeID INT NOT NULL,
-   UnstrungLength FLOAT,
-   StrungLength FLOAT,
-   MinBoxDimLength FLOAT,
-   MinBoxWidth FLOAT,
-   MinBoxDepth FLOAT,
-   SiyahEffectiveTopLength FLOAT,
-   SiyahEffectiveBottomLength FLOAT,
-   SiyahEffectiveTopAngle FLOAT,
-   SiyahEffectiveBottomAngle FLOAT,
-   BowMass INT,
-   GripLength FLOAT,
-   GripWidth FLOAT,
-   GripDepth FLOAT,
-   MaxLimbThickness FLOAT,
-   MinLimbThickness FLOAT,
-   MaxLimbWidth FLOAT,
-   MinLimbWidth FLOAT,
-   ArrowPassWidth FLOAT,
-   MaxDraw INT,
-   StockStringLengthMin FLOAT,
-   StockStringLengthMax FLOAT,
-   BraceHeight FLOAT,
-   ContributorContactType VARCHAR(255),
-   ContributorContactInfo VARCHAR(255),
-   ManufactureDate VARCHAR(255),
-   MeasurementDate VARCHAR(255),
-   Comments TEXT,
-   Asym BOOLEAN,
-   AsymLengthTop FLOAT,
-   AsymLengthBottom FLOAT,
-   DfData JSONB,
-   CentralDifferences JSONB,
-   RegressionEstimation JSONB NOT NULL,
-   LongbowPoint INT,
-   StoredEnergy FLOAT[][],
-   FpsData JSONB[],
-   DfHash VARCHAR(255) UNIQUE
+CREATE TABLE samples (
+   sample_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   bow_type_id INT NOT NULL REFERENCES bow_types(bow_type_id),
+   unstrung_length FLOAT,
+   strung_length FLOAT,
+   min_box_dim_length FLOAT,
+   min_box_width FLOAT,
+   min_box_depth FLOAT,
+   siyah_effective_top_length FLOAT,
+   siyah_effective_bottom_length FLOAT,
+   siyah_effective_top_angle FLOAT,
+   siyah_effective_bottom_angle FLOAT,
+   bow_mass INT,
+   grip_length FLOAT,
+   grip_width FLOAT,
+   grip_depth FLOAT,
+   max_limb_thickness FLOAT,
+   min_limb_thickness FLOAT,
+   max_limb_width FLOAT,
+   min_limb_width FLOAT,
+   arrow_pass_width FLOAT,
+   max_draw INT,
+   stock_string_length_min FLOAT,
+   stock_string_length_max FLOAT,
+   brace_height FLOAT,
+   contributor_contact_type TEXT,
+   contributor_contact_info TEXT,
+   manufacture_date TEXT,
+   measurement_date TEXT,
+   comments TEXT,
+   asym BOOLEAN,
+   asym_length_top FLOAT,
+   asym_length_bottom FLOAT,
+   df_data PATH NOT NULL,
+   df_data_text TEXT GENERATED ALWAYS AS (df_data::text) STORED UNIQUE,
+   central_differences PATH,
+   longbow_point INT,
+   stored_energy FLOAT[][],
+   regression_curve PATH, 
+   coeffs FLOAT[],
+   regression_eqn TEXT, 
+   regression_derivative TEXT, 
+   regression_derivative_values PATH, 
+   hysteresis FLOAT,
+   virtual_mass FLOAT
 );
+
+CREATE TABLE fps_data (
+   fps_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   sample_id INT NOT NULL REFERENCES samples(sample_id),
+   dl FLOAT,
+   arrow_weight FLOAT,
+   gpp FLOAT,
+   fps FLOAT,
+   measured_energy FLOAT,
+   stored_energy FLOAT,
+   efficiency FLOAT,
+   draw_length_to_belly FLOAT
+);
+
+CREATE TABLE fps_regression_data (
+   regression_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   sample_id INT NOT NULL REFERENCES samples(sample_id),
+   dl FLOAT,
+   coefficients FLOAT[],
+   fitted_line PATH
+);
+
+
+
+
+-- pg_ctl -D "C:\Program Files\PostgreSQL\15\data" start
+-- C:/Users/liria/code/asiatic-bow-data-dump/pgsql/initialization.sql
+
--- utils/insertToPsql.js
+++ utils/insertToPsql.js
@@ -2,17 +2,22 @@ require('dotenv').config();
 const pgp = require('pg-promise')();
 const fs = require('fs');
 const path = require('path');
-const crypto = require('crypto');
 
-const client = pgp({
-  connectionString: 'postgres://liria:1234@localhost:5433/test_db',
-});
+dbConfig = {
+  host: 'localhost',
+  port: 5433,
+  database: 'test_db',
+  user: 'postgres',
+};
 
 const dataPath = path.join(__dirname, '../', 'data', 'bows');
 
 async function main() {
+  const db = pgp(dbConfig);
+
   console.log(`Looking for subfolders in ${dataPath}`);
   const bowFolders = fs.readdirSync(dataPath);
+
   for (const folder of bowFolders) {
     console.log(`Found subfolder ${folder}`);
     const filePath = path.join(dataPath, folder, 'data.json');
@@ -24,171 +29,11 @@ async function main() {
     const bowLink = jsonData['bow-link'];
     const samples = jsonData['samples'];
 
-    // Insert BowType or do nothing if it already exists
-    const bowTypeRes = await client.oneOrNone(
-      'INSERT INTO BowTypes(BowTitle, BowLink) VALUES($1, $2) ON CONFLICT (BowTitle) DO NOTHING RETURNING BowTypeID',
-      [bowTitle, bowLink],
-    );
-    let bowTypeId = bowTypeRes ? bowTypeRes.bowtypeid : null;
-
-    if (!bowTypeId) {
-      // If BowType was not inserted, it means it already exists. Fetch its BowTypeID.
-      const existingBowType = await client.one(
-        'SELECT BowTypeID FROM BowTypes WHERE BowTitle = $1',
-        [bowTitle],
-      );
-      console.log(existingBowType);
-      bowTypeId = existingBowType.bowtypeid;
-    }
+    const bowTypeData = buildBowTypeQuery(bowTitle, bowLink);
+    const bowTypeId = await insertBowTypeAndGetId(db, bowTypeData);
 
     for (const sample of samples) {
-      const dfDataHash = crypto
-        .createHash('sha256')
-        .update(JSON.stringify(sample['df-data']))
-        .digest('hex');
-      const existingSample = await client.oneOrNone(
-        'SELECT * FROM Samples WHERE DfHash = $1',
-        [dfDataHash],
-      );
-
-      if (!existingSample) {
-        if (sample['contributed-by']) {
-          sample['contribution-type'] = Object.keys(
-            sample['contributed-by'],
-          )[0];
-          sample['contributed-by'] =
-            sample['contributed-by'][sample['contribution-type']];
-        } else {
-          sample['contribution-type'] = 'N/A';
-          sample['contributed-by'] = 'N/A';
-        }
-
-        const values = {
-          bowTypeId: bowTypeId,
-          unstrungLength: sample['unstrung-length'],
-          strungLength: sample['strung-length'],
-          minBoxDimLength: sample['min-box-dim'].length,
-          minBoxWidth: sample['min-box-dim'].width,
-          minBoxDepth: sample['min-box-dim'].depth,
-          siyahEffectiveTopLength: sample.siyahs['effective-top-length'],
-          siyahEffectiveBottomLength: sample.siyahs['effective-bottom-length'],
-          siyahEffectiveTopAngle: sample.siyahs['effective-top-angle'],
-          siyahEffectiveBottomAngle: sample.siyahs['effective-bottom-angle'],
-          bowMass: sample['bow-mass'],
-          gripLength: sample['grip-dim'].length,
-          gripWidth: sample['grip-dim'].width,
-          gripDepth: sample['grip-dim'].thickness,
-          maxLimbThickness: sample['max-limb-thickness'],
-          minLimbThickness: sample['min-limb-thickness'],
-          maxLimbWidth: sample['max-limb-width'],
-          minLimbWidth: sample['min-limb-width'],
-          arrowPassWidth: sample['arrow-pass-width'],
-          maxDraw: sample['max-draw'],
-          stockStringLengthMin: sample['stock-string-length'].min,
-          stockStringLengthMax: sample['stock-string-length'].max,
-          braceHeight: sample['brace-height'],
-          contributionType: sample['contribution-type'],
-          contributedBy: sample['contributed-by'],
-          manufactureDate: sample['manufacture-date'],
-          measurementDate: sample['measurement-date'],
-          comments: sample.comments,
-          asym: sample.asym,
-          asymLengthTop: sample['asym-length'].top,
-          asymLengthBottom: sample['asym-length'].bottom,
-          dfData: JSON.stringify(sample['df-data']),
-          centralDifferences: JSON.stringify(sample['central-differences']),
-          regressionEstimation: JSON.stringify(sample['regression-estimation']),
-          longbowPoint: sample['longbow-point'],
-          storedEnergy: convertStoredEnergyToArray(sample['stored-energy']),
-          fpsData: sample['fps-data'],
-          dfDataHash: dfDataHash,
-        };
-
-        const insertSampleQuery = `
-  INSERT INTO Samples(
-    BowTypeID, 
-    UnstrungLength, 
-    StrungLength, 
-    MinBoxDimLength, 
-    MinBoxWidth, 
-    MinBoxDepth, 
-    SiyahEffectiveTopLength, 
-    SiyahEffectiveBottomLength, 
-    SiyahEffectiveTopAngle, 
-    SiyahEffectiveBottomAngle, 
-    BowMass, 
-    GripLength, 
-    GripWidth, 
-    GripDepth, 
-    MaxLimbThickness, 
-    MinLimbThickness, 
-    MaxLimbWidth, 
-    MinLimbWidth, 
-    ArrowPassWidth, 
-    MaxDraw, 
-    StockStringLengthMin, 
-    StockStringLengthMax, 
-    BraceHeight, 
-    ContributorContactType, 
-    ContributorContactInfo, 
-    ManufactureDate, 
-    MeasurementDate, 
-    Comments, 
-    Asym, 
-    AsymLengthTop, 
-    AsymLengthBottom, 
-    DfData, 
-    CentralDifferences, 
-    RegressionEstimation, 
-    LongbowPoint, 
-    StoredEnergy,
-    DfHash,
-    FpsData
-  ) VALUES(
-    $(bowTypeId), 
-    $(unstrungLength), 
-    $(strungLength), 
-    $(minBoxDimLength), 
-    $(minBoxWidth), 
-    $(minBoxDepth), 
-    $(siyahEffectiveTopLength), 
-    $(siyahEffectiveBottomLength), 
-    $(siyahEffectiveTopAngle), 
-    $(siyahEffectiveBottomAngle), 
-    $(bowMass), 
-    $(gripLength), 
-    $(gripWidth), 
-    $(gripDepth), 
-    $(maxLimbThickness), 
-    $(minLimbThickness), 
-    $(maxLimbWidth), 
-    $(minLimbWidth), 
-    $(arrowPassWidth), 
-    $(maxDraw), 
-    $(stockStringLengthMin), 
-    $(stockStringLengthMax), 
-    $(braceHeight), 
-    $(contributionType),
-    $(contributedBy), 
-    $(manufactureDate), 
-    $(measurementDate), 
-    $(comments), 
-    $(asym), 
-    $(asymLengthTop), 
-    $(asymLengthBottom), 
-    $(dfData), 
-    $(centralDifferences), 
-    $(regressionEstimation), 
-    $(longbowPoint), 
-    $(storedEnergy),
-    $(fpsData),
-    $(dfDataHash)
-  )`;
-
-        await client.none(insertSampleQuery, values).catch((error) => {
-          console.log('ERROR:', error);
-        });
-      }
+      buildSampleQuery(sample);
     }
   }
 }
@@ -202,7 +47,173 @@ main()
     pgp.end();
   });
 
-function convertStoredEnergyToArray(storedEnergy) {
-  // Convert dict of 3 values to array of 3 pairs instead
-  return Object.entries(storedEnergy);
+function buildBowTypeQuery(bowTitle, bowLink) {
+  console.log(
+    `Building BowType Insertion Query for bow with title ${bowTitle}`,
+  );
+  const manufacturers = [
+    'AF Archery',
+    'Alibow',
+    'Arcus',
+    'Bearpaw',
+    'Dae Han',
+    'Daylite',
+    'MR',
+    'Elong',
+    'Flagella',
+    'Grozer',
+    'Flagella Dei',
+    'Grozer',
+    'Higo Souzan',
+    'Jackal',
+    'Javaman',
+    'Koyama',
+    'JZW',
+    'Kassai',
+    'Kaya',
+    'Liang Zhi',
+    'Magor',
+    'Mariner',
+    'Medicine Bows',
+    'Misko Rovcanin',
+    'Nawalny',
+    'Numair',
+    'Paragon',
+    'Samick',
+    'Sarmat',
+    'Spearman',
+    'Simsek',
+    'Vegh',
+    'Yuwen',
+  ];
+  let manufacturer;
+  let modelName;
+  for (let i = 0; i < manufacturers.length; i++) {
+    if (bowTitle.startsWith(manufacturers[i])) {
+      manufacturer = manufacturers[i];
+    }
+  }
+  if (manufacturer === undefined) {
+    manufacturer = 'Other';
+    console.log(`Bow title: ${bowTitle}`);
+    modelName = bowTitle.trim();
+  } else {
+    modelName = bowTitle.replace(manufacturer, '').trim();
+  }
+
+  return {
+    manufacturer,
+    model_name: modelName,
+    bow_link: bowLink,
+  };
 }
+
+function buildSampleQuery(sample, bowTypeId) {
+  const sampleQuery = {};
+  sampleQuery['bow_type_id'] = bowTypeId;
+  sampleQuery['unstrung_length'] = sample['unstrung-length'];
+  sampleQuery['strung_length'] = sample['strung-length'];
+  sampleQuery['min_box_dim_length'] = sample['min-box-dim']['length'];
+  sampleQuery['min_box_dim_width'] = sample['min-box-dim']['width'];
+  sampleQuery['min_box_dim_depth'] = sample['min-box-dim']['depth'];
+  sampleQuery['siyah_effective_top_length'] =
+    sample['siyahs']['effective-top-length'];
+  sampleQuery['siyah_effective_bottom_length'] =
+    sample['siyahs']['effective-bottom-length'];
+  sampleQuery['siyah_effective_top_angle'] =
+    sample['siyahs']['effective-top-angle'];
+  sampleQuery['siyah_effective_top_angle'] =
+    sample['siyahs']['effective-top-angle'];
+  sampleQuery['bow_mass'] = sample['bow-mass'];
+  sampleQuery['grip_length'] = sample['grip-dim']['length'];
+  sampleQuery['grip_width'] = sample['grip-dim']['width'];
+  sampleQuery['grip_depth'] = sample['grip-dim']['depth'];
+  sampleQuery['max_limb_thickness'] = sample['max-limb-thickness'];
+  sampleQuery['min_limb_thickness'] = sample['min-limb-thickness'];
+  sampleQuery['max_limb_width'] = sample['max-limb-width'];
+  sampleQuery['min_limb_width'] = sample['min_limb_width'];
+  sampleQuery['arrow_pass_width'] = sample['arrow-pass-width'];
+  sampleQuery['max_draw'] = sample['max-draw'];
+  sampleQuery['stock_string_length_min'] = sample['stock-string-length-min'];
+  sampleQuery['stock_string_length_max'] = sample['stock-string-length-max'];
+  sampleQuery['brace_height'] = sample['brace-height'];
+  if (sample['contributed-by']) {
+    const contributors = getContributor(sample['contributed-by']);
+    sampleQuery['contributor_contact_type'] = contributors[0];
+    sampleQuery['contributor_contact_info'] = contributors[1];
+  }
+  sampleQuery['manufacture_date'] = sample['manufacture-date'];
+  sampleQuery['measurement_date'] = sample['measurement-date'];
+  sampleQuery['comments'] = sample['comments'];
+  sampleQuery['asym'] = sample['asym'];
+  sampleQuery['asym_length_top'] = sample['asym-length']['top'];
+  sampleQuery['asym_length_bottom'] = sample['asym-length']['bottom'];
+  sampleQuery['df_data'] = pointArrayToPath(sample['df-data']);
+  sampleQuery['central_differences'] = pointArrayToPath(
+    sample['central-differences'],
+  );
+  sampleQuery['longbow_point'] = sample['longbow-point'];
+
+  if (sample['stored-energy']) {
+    sampleQuery['stored_energy'] = convertStoredEnergyToArray(
+      sample['stored-energy'],
+    );
+  }
+
+  if (sample['regression-estimation'] !== undefined) {
+    sampleQuery['regression_curve'] = pointArrayToPath(
+      sample['regression-estimation']['df-curve'],
+    );
+    sampleQuery['coeffs'] = sample['regression-estimation']['coeffs'];
+    sampleQuery['regression_eqn'] =
+      sample['regression-estimation']['regression-eqn'];
+    sampleQuery['regression_derivative'] =
+      sample['regression-estimation']['regression-derivative'];
+    sampleQuery['regression_derivative_values'] = pointArrayToPath(
+      sample['regression-estimation']['regression-derivative-values'],
+    );
+    sampleQuery['hysteresis'] = sample['hysteresis'];
+    sampleQuery['virtual_mass'] = sample['virtual-mass'];
+  }
+}
+
+async function insertBowTypeAndGetId(db, bowTypeData) {
+  try {
+    // Check if the combination of model_name and manufacturer already exists
+    const existingBowType = await db.oneOrNone(
+      'SELECT bow_type_id FROM bow_types WHERE model_name = $1 AND manufacturer = $2',
+      [bowTypeData.model_name, bowTypeData.manufacturer],
+    );
+
+    if (existingBowType) {
+      // If it exists, return the existing id
+      return existingBowType.bow_type_id;
+    } else {
+      // If it doesn't exist, insert the new row
+      const query =
+        pgp.helpers.insert(bowTypeData, null, 'bow_types') +
+        ' RETURNING bow_type_id';
+      const result = await db.one(query);
+
+      // Return the id of the new row
+      return result.bow_type_id;
+    }
+  } catch (error) {
+    console.error('Error inserting bow type:', error);
+    throw error;
+  }
+}
+
+function getContributor(contributedBy) {
+  return Object.entries(contributedBy);
+}
+
+function pointArrayToPath(dfData) {
+  return `((${dfData.map((point) => `${point.x},${point.y}`).join('),(')}))`;
+}
+
+function convertStoredEnergyToArray(storedEnergy) {
+  Object.entries(storedEnergy).map(([key, value]) => [parseFloat(key), value]);
+}
+
+function buildFpsQuery(fpsData) {}
